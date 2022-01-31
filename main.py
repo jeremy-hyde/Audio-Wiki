8933d8b7f91c0de1ee119b5d53e46dd4b796d78e
jeremy <ferrer.jeremy3@gmail.com>
2022-01-31 17:31:32 +0100

    wip 2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from scraper import PyCurlSynchronousEngine, Request
+import os
+
 from lxml import etree
+from scraper import PyCurlSynchronousEngine, Request
 
 
 def main():
     with PyCurlSynchronousEngine() as engine:
         res = engine.send(Request('https://en.wikipedia.org/wiki/Elizabeth_II'))
+        print(res)
 
         title = res.xfirst('//h1/text()')
         chapters = []
@@ -12,7 +15,7 @@ def main():
         # Get Intro
         chapters.append({
             'name': 'Introduction',
-            'content': res.xall("//table[contains(@class,'vcard')]/following-sibling::h2[1]/preceding-sibling::p")
+            'content': res.xall("//table[contains(@class,'vcard')]/following-sibling::h2[1]/preceding-sibling::*[self::p or self::blockquote]")
         })
 
         # Get Chapter names, excluding the last ones (except the first encounter that will be used as delimiter)
@@ -27,42 +30,65 @@ def main():
         for prev_h2, next_h2 in zip(h2s_names, h2s_names[1:]):
             chapters.append({
                 'name': prev_h2,
-                'content': res.xall("//h2[./span/text()='{}']/following-sibling::p[following-sibling::h2[./span/text()='{}']]".format(prev_h2, next_h2))
+                'content': res.xall("//h2[./span/text()='{}']/following-sibling::*[self::p or self::blockquote][following-sibling::h2[./span/text()='{}']]".format(prev_h2, next_h2))
             })
 
-        # from pprint import pprint
-        # pprint(chapters)
-
-        # etree.tostring(ele, pretty_print=True).decode()
+        #print(etree.tostring(chapters[3]['content'], pretty_print=True).decode())
 
         # Extract sub chapter (h3)
         # TODO
 
-        # Clean Up each chapter
+        # Remove table TODO
+        # Remove left over tags TODO
+        # Clean up each chapter
+        # Reformat link
+        # Reformat italique
+        # Reformat quotes
         # Remove References
         for chapter in chapters:
             for sub_chapter in chapter['content']:
-                for link in sub_chapter.xpath('//a'):
-                    link.drop_tree()
+                for link in sub_chapter.xpath('.//a'):
+                    link.drop_tag()
 
-                    print(sub_chapter.xpath('./text()'))
+                for i in sub_chapter.xpath('.//i'):
+                    i.drop_tag()
+
+                for quote in sub_chapter.xpath('.//blockquote'):
+                    print(quote.xpath('./p/text()'))
+                    quote.drop_tag()
+
+                for sup in sub_chapter.xpath('.//sup'):
+                    sup.drop_tree()
+
+        # Create directory
+        try:
+            os.mkdir("var/{}".format(title))
+        except FileExistsError:
+            pass
+
+        with open('var/{}/raw.txt'.format(title), mode='w') as file:
+            for chapter in chapters:
+                if chapter['content']:
+                    file.write(chapter['name'].strip())
+                    file.write('\n\n')
+                    for sub_chapter in chapter['content']:
+                        if sub_chapter.xfirst('./text()'):
+                            file.write(sub_chapter.xfirst('./text()'))
+                            file.write('\n\n')
+
+                    file.write('\n\n')
 
 
         # Get all images
         # Can be done on the root
 
 
-        # Replace link
-        # Remove table
-        # Remove left over tags
+
 
         # Add ssml to title and chapters
 
         # Write file
 
-        print(content)
-        print(title, file=open('output.txt', mode='a'))
-        print(intro, file=open('output.txt', mode='a'))
 
 
 
