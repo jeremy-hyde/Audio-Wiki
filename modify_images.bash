926bcb25f01ae693d4fc8376cfc9873de0c3e162
Ferrer-Jeremy <ferrer.jeremy3@gmail.com>
2022-02-06 23:23:07 +0100

    Fix the svg conversion

--- modify_images.bash
+++ modify_images.bash
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
+
+#set -e
 
 if [ ! -d "$1"/images_backup/ ]; then
     echo '=> Backup Images'
+    rm
     cp -Ra "$1"/images/ "$1"/images_backup/
 fi
 
@@ -10,10 +14,13 @@ for filename in "$1"/images/*; do
     line=$(sed -n "$ITER"p "$1"/images.txt)
 
     # Convert SVG to PNG with the right size
-    if [[ $filename =~ "*.svg" ]]; then
-        #[Ref](https://legacy.imagemagick.org/discourse-server/viewtopic.php?t=31327)
-        mogrify -resize 1920x880 -density 400 -format png "$1"/images/*.svg -channel RGB  "$1"/images/*.png
-        rm "$1"/images/*.svg
+    if [[ $filename =~ .*\.svg ]]; then
+      echo "Convert svg to png"
+        # https://legacy.imagemagick.org/discourse-server/viewtopic.php?t=31327
+        filenamepng="${filename/svg/png}"
+        convert -resize 1920x880 -density 400 -format png "$filename" -channel RGB "$filenamepng"
+        rm "$filename"
+        filename=$filenamepng
     fi
 
     echo $ITER
@@ -22,6 +29,7 @@ for filename in "$1"/images/*; do
 
     echo $filename
     # Add Caption with wrap at 1920 and a lot of small things
+    # https://legacy.imagemagick.org/Usage/thumbnails/#labels
     convert "$filename" \( -background black -fill white -pointsize 25 -size 1920x -gravity Center caption:"$line" -trim +repage -bordercolor black -border 50 \) -gravity South -append "$filename"
 
     echo $line
