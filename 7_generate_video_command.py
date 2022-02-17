a084d14d8bb117f87f4308abc4dc107467d0d172
Ferrer-Jeremy <ferrer.jeremy3@gmail.com>
2022-02-17 14:43:55 +0100

    Finish video generation script

--- 7_generate_video_command.py
+++ 7_generate_video_command.py
@@ -1,5 +1,5 @@
 import argparse
-
+import os
 
 """
 ffmpeg \
@@ -19,28 +19,54 @@ out2.mkv
 """
 
 base_command = """
-ffmpeg \
--f lavfi -i color=c=black:s=1920x1080:r=24 \
--i {audio_path} \
-{images_paths}
--filter_complex "\
-{images_overlays}
--c:a copy \
--pix_fmt yuvj422p \
--crf 17 \
--shortest \
-{path}/output.mp4
+ffmpeg \\
+-f lavfi -i color=c=black:s=1920x1080:r=24 \\
+-i {audio_path} \\
+{images_paths} \\
+-filter_complex "\\
+{images_overlays}" \\
+-c:a aac \\
+-pix_fmt yuvj422p \\
+-crf 17 \\
+-shortest \\
+"{folder}/output.mp4"
 """
 
 
 def main(folder):
+    images_timepoints = '{}/timepoints/images.txt'.format(folder)
+    images_filenames = sorted(next(os.walk('{}/images'.format(folder)), (None, None, []))[2])
+    audio_duration = round(float(os.popen('soxi -D "{}/audio/full.wav"'.format(folder)).read().strip()), 2)
+
+    images_paths = []
+    images_overlays = []
+
+    images_timepoints_map = {}
+
+    with open(images_timepoints, mode='r') as file:
+        for line in file:
+            name, seconds = line.split(' => ')
+            number = name[3:]  # Keep the last 3 char
+            images_timepoints_map[number] = round(float(seconds), 2)
+
+    start_timestamp = 0
+    for image in images_filenames:
+        images_paths.append('-i "{}/images/{}"'.format(folder, image))
+        next_number = int(image.split('.')[0]) + 1
+        end_timestamp = images_timepoints_map.get('{}'.format(next_number))
+        if not end_timestamp:
+            end_timestamp = audio_duration
+
+        images_overlays.append("overlay=enable='between(t,{},{})'".format(start_timestamp, end_timestamp))
+        start_timestamp = end_timestamp
+
     arguments = {
-        'audio_path': '{}/audio/full.wav'.format(folder),
-        'images_paths': [],
-        'images_overlays': [],
-        'path': folder,
+        'audio_path': '"{}/audio/full.wav"'.format(folder),
+        'images_paths': ' \\\n'.join(images_paths),
+        'images_overlays': ',\\\n'.join(images_overlays),
+        'folder': folder,
     }
-    print(base_command.format(**arguments))
+    print(base_command.format(**arguments).strip())
 
 
 if __name__ == '__main__':
